81ce79253f6dc21021d6b194824f873ea43c8958
TheStressTest <73505616+TheStressTest@users.noreply.github.com>
2020-12-13 21:29:21 -0800

    .

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+____  __.__  __                              
+|    |/ _|__|/  |_  ________ __  ____   ____  
+|      < |  \   __\/  ___/  |  \/    \_/ __ \ 
+|    |  \|  ||  |  \___ \|  |  /   |  \  ___/ 
+|____|__ \__||__| /____  >____/|___|  /\___  >
+        \/             \/           \/     \/ 
+"""
+
 import sys, traceback
 import discord
 from discord.ext import commands
@@ -7,7 +16,9 @@ import json
 import math
 import random
 
-
+with open('secrets.json', 'r') as tf:
+    tf = json.load(tf)
+    TOKEN = tf['token']
 
 utilities_extensions = [
     'cogs.utilities.serverinfo',
@@ -16,6 +27,7 @@ utilities_extensions = [
 moderation_extensions = ['cogs.mod.kick', 'cogs.mod.ban', 'cogs.mod.tempmute','cogs.mod.mute','cogs.mod.unmute','cogs.mod.slowmode', 'cogs.mod.warn']
 reddit_extensions = ['cogs.reddit.meme', 'cogs.reddit.aww', 'cogs.reddit.gaming']
 fun_extensions = ['cogs.fun.eightball', 'cogs.fun.ttt']
+
 bot = commands.Bot(command_prefix='k!')
 
 bot.remove_command("help")
@@ -30,13 +42,9 @@ if __name__ == '__main__':
     for extension in fun_extensions:
         bot.load_extension(extension)
 
-
-
 @bot.command()
 async def test(ctx):
    await ctx.send('<:kitsuneheart2:786989004272173106> __**Thank you for inviting me!**__\n<:kitsunethink:786984788095533069> My current prefix is **`k!`**, if you would like to view my commands, please type `k!help`!\n<:kitsuneheart:786984788761509908> If you like this bot, make sure to vote us on **top.gg!**, https://top.gg/bot/768967985326456874 ')
-
-
 
 @bot.event
 async def on_ready():
@@ -46,62 +54,11 @@ async def on_ready():
 
 
 
-
-
-
-
 @bot.event
 async def on_command_error(ctx, error):
   await ctx.send(f"<:kitsunethink:786984788095533069> {error}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@bot.command(pass_context=True)
+@bot.command()
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def bal(ctx):
     await open_account(ctx.author)
@@ -117,13 +74,9 @@ async def bal(ctx):
     
 
     embed = discord.Embed(title=f"{ctx.author.name}'s Balance",description=f"**Wallet:** {wallet_amt}\n**Bank:** {bank_amt}\n**Total:** {total_amt}", color=0xffa500)
-
-
     await ctx.send(embed = embed)
 
-
-
-@bot.command(pass_context=True)
+@bot.command()
 @commands.cooldown(1, 30, commands.BucketType.user)
 async def beg(ctx):
     await open_account(ctx.author)
@@ -134,20 +87,16 @@ async def beg(ctx):
 
     earnings = random.randrange(101)
 
-    await ctx.send(f"**{random.choice} gave you {earnings} coins!!**")
+    await ctx.send(f"**kitsune gave you {earnings} koins!!**")
 
     users[str(user.id)]["wallet"] += earnings
 
     with open("mainbank.json", "w") as f:
         json.dump(users,f)
-
-
 @bot.command()
 async def credits(ctx):
   await ctx.send("<@738604939957239930> | **Owner of the bot, developed like 85% of it.**\n <@701494621162963044> | **Helped with the creation of the warning system.**\n <@474016258019557377> | **Worked on the kitsune! dashboard.** (in progress)")
-
-
-@bot.command(pass_context=True, aliases=["with",'w'])
+@bot.command(aliases=["with",'w'])
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def withdraw(ctx,amount = None):
     await open_account(ctx.author)
@@ -171,7 +120,7 @@ async def withdraw(ctx,amount = None):
 
     await ctx.send(f"You've withdrew **{amount} Koins**!")
 
-@bot.command(pass_context=True)
+@bot.command()
 async def dep(ctx,amount = None):
     await open_account(ctx.author)
     if amount == None:
@@ -185,7 +134,7 @@ async def dep(ctx,amount = None):
     if amount>bal[0]:
         await ctx.send("You don't have that much money!")
         return
-    if amount<0:
+    if amount<=0:
         await ctx.send("Amount must be positive.")
         return
 
@@ -193,9 +142,7 @@ async def dep(ctx,amount = None):
     await update_bank(ctx.author,amount,"bank")
 
     await ctx.send(f"**You've deposited **{amount}** Koins!")
-
-
-@bot.command(pass_context=True)
+@bot.command()
 @commands.cooldown(1, 120, commands.BucketType.user)
 async def rob(ctx,member: discord.Member):
     await open_account(ctx.author)
@@ -218,8 +165,6 @@ async def rob(ctx,member: discord.Member):
     await ctx.send(f"You've robbed **{member.name}** and gained **{earnings} Koins**!")
 
 
-
-
 async def open_account(user):
 
     users = await get_bank_data()
@@ -233,15 +178,11 @@ async def open_account(user):
     with open("mainbank.json", "w") as f:
         json.dump(users,f)
     return True
-
-
 async def get_bank_data():
     with open("mainbank.json", "r") as f:
         users = json.load(f)
 
     return users
-
-
 
 async def update_bank(user, change=0,mode = 'wallet'):
 
@@ -253,17 +194,9 @@ async def update_bank(user, change=0,mode = 'wallet'):
         json.dump(users,f)
 
     bal = users[str(user.id)]["wallet"],users[str(user.id)]["bank"]
-
-
     return bal
 
-
-
 bot.run(
-    'NzY4OTY3OTg1MzI2NDU2ODc0.X5ILPg.B0Fs0zyLhXml0wbx4xw9Fdbn8nE',
+    TOKEN,
     bot=True,
     reconnect=True)
-
-
-
-
