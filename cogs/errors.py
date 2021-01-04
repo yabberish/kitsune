655ca6ead05335a6e9360c44dfa6922aaf841f21
TheStressTest <73505616+TheStressTest@users.noreply.github.com>
2021-01-04 10:58:34 -0800

    merged most cogs into one file, IE: 4 files in mod folder go into one file called mod.py

--- cogs/errors.py
+++ cogs/errors.py
@@ -1,12 +1,10 @@
-# -*- coding: utf-8 -*-
-
 import discord
 import traceback
 import sys
 from discord.ext import commands
 
 
-class ErrorHandler(commands.Cog):
+class ErrorHandler(commands.Cog, command_attrs=dict(hidden=True)):
 
     def __init__(self, bot):
         self.bot = bot
@@ -33,6 +31,7 @@ class ErrorHandler(commands.Cog):
         else:
             print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
             traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
 
 def setup(bot):
     bot.add_cog(ErrorHandler(bot))
