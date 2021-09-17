d1322b260875c9f4d9276680a8d0f6cdaad0361b
elf <49889930+elfq@users.noreply.github.com>
2021-09-17 19:58:51 -0400

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -6,7 +6,7 @@ import aiohttp
 
 def is_voter():
     async def predicate(ctx):
-        top_gg = dbl.DBLClient(bot=ctx.bot, token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc2ODk2Nzk4NTMyNjQ1Njg3NCIsImJvdCI6dHJ1ZSwiaWF0IjoxNjEwNDczNDUyfQ.Ro-kwtzfbcPcbBSpijtvBe1kKkB9mBj2xSUts670JtE', session=aiohttp.ClientSession())
+        top_gg = dbl.DBLClient(bot=ctx.bot, token='', session=aiohttp.ClientSession())
         print(await top_gg.get_user_vote(ctx.author.id))
         if await top_gg.get_user_vote(user_id=ctx.author.id):
             return True
